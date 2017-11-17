31d0364164857c2f2bb4fd4b6b7bf163ea1ff425
jvidalmoreira <jvidalmoreira@victoria20.danielcastelao.org>
2017-11-17 09:17:01 +0100

    commit B segundo commit

--- src/examen/Examen.java
+++ src/examen/Examen.java
@@ -15,7 +15,7 @@ public class Examen {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println(" realizo el primer commit");
+        System.out.println(" realizo el segundo  commit");
     }
     
 }
