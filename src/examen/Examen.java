fa29187354a1a882d7636fe8068012020987135d
jvidalmoreira <jvidalmoreira@victoria20.danielcastelao.org>
2017-11-17 09:24:57 +0100

    commit C tercer commit

--- src/examen/Examen.java
+++ src/examen/Examen.java
@@ -15,7 +15,8 @@ public class Examen {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println(" realizo el segundo  commit");
+        System.out.println(" realizo el segundo commit");
+        System.out.println("realizo el primer commit de la segunda rama");
     }
     
 }
